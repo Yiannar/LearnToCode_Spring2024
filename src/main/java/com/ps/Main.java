089e42a9d8695817ef17487c5098741b8f9ccbcd
Yianna <Yiannarichey@pursuit.org>
2024-04-15 13:30:31 -0400

    first commit

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,22 @@
+package com.ps;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Understanding the problem
+            // Inputs
+            // Outputs
+
+        // Make a plan -
+            // pseudocode
+
+        // Do it!/ Code!
+
+        // Look back
+            // Optimizing/refactoring
+
+
+    }
+}
+
