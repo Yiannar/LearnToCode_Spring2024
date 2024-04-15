385f4d3b825eaa65bfaae2a48f21e6fbead82a0f
Yianna <Yiannarichey@pursuit.org>
2024-04-15 13:57:18 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Math Calculator
+
+## Understanding the problem
+- Inputs
+- User input
+- Outputs
+- CLI
+- Creating a calculator with the add, sub, div, multi, modulo methods and a user interface
+
+
+
+  // Inputs
+  // Using Scanner to take an initial user menu command
+  // First asking for what calc they would like to use, basic, scientific
+  //  Outputs
+  // Print to CLI the result of calculation
+  // pseudocode
+
+#  // Do it!/ Code!
+
+        // Look back
+            // Optimizing/refactoring
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -6,9 +6,23 @@ class Main {
 
         // Understanding the problem
             // Inputs
+                // User input
             // Outputs
+                // CLI
+
+            // Creating a calculator
+                // creating an interface for a user
+                    // Create a menu with user input
+                // Add, sub, div, multi methods
+
+            // Creating a calculator with the add, sub, div, multi, modulo methods and a user interface
 
         // Make a plan -
+            // Inputs
+                // Using Scanner to take an initial user menu command
+                // First asking for what calc they would like to use, basic, scientific
+            //  Outputs
+                // Print to CLI the result of calculation
             // pseudocode
 
         // Do it!/ Code!
